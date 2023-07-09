9b4c3a7f8bb89beb2804e95cfae74df964043735
Mylena <myvendramini@gmail.com>
2023-07-09 09:39:10 +0100

    test

--- server/routes/activities.js
+++ server/routes/activities.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const activitiesController = require('../controllers/activities');
 
-// console.log('test')
 
 router.get('/activities', activitiesController.getActivities);
 router.post('/activities', activitiesController.createActivity);
